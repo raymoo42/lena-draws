b6f95fd9a5aa9c49570197ee71bc51a46c0f0ab5
Your Name <you@example.com>
2024-03-23 16:51:08 +0100

    Stuff

--- frontend/lena-draws/src/app/app.routes.ts
+++ frontend/lena-draws/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ImageUploadComponent } from './image-upload/image-upload.component';
+import { TimeLineComponent } from './time-line/time-line.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ImageUploadComponent },
+  { path: 'timeline', component: TimeLineComponent },
+];
---
+++ frontend/lena-draws/src/app/image-upload/image-upload.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-image-upload',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './image-upload.component.html',
+  styleUrl: './image-upload.component.css'
+})
+export class ImageUploadComponent {
+
+}
